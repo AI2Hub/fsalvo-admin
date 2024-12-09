d383b8c876120dd6f923f6cb2dedb1ce3945663c
koobe <koobe.liu@uaf.com.cn>
2024-12-09 11:08:01 +0800

    Router::new()优化

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use std::env;
 
 use log::info;
-use salvo::affix_state;
 use salvo::prelude::*;
 use sea_orm::{Database, DatabaseConnection};
 
-use handler::system::menu_handler::{*};
-use handler::system::role_handler::{*};
-use handler::system::user_handler::{*};
+use crate::routes::{build_other_route, build_system_route};
+use handler::system::user_handler::*;
 use middleware::auth::auth_token;
 
-pub mod model;
-pub mod vo;
-pub mod handler;
-pub mod utils;
-mod middleware;
 mod common;
+pub mod handler;
+mod middleware;
+pub mod model;
+mod routes;
+pub mod utils;
+pub mod vo;
 
 #[handler]
 async fn hello() -> &'static str {
@@ -38,30 +37,20 @@ async fn main() {
     let state = AppState { conn };
 
     info!("{:?}", state.conn);
-    let router = Router::new().hoop(affix_state::inject(state)).path("/api").get(hello)
-        .push(Router::new().path("login").post(login))
-        .push(
-            Router::new().hoop(auth_token)
-                .push(Router::new().path("query_user_role").post(query_user_role))
-                .push(Router::new().path("update_user_role").post(update_user_role))
-                .push(Router::new().path("query_user_menu").get(query_user_menu))
-                .push(Router::new().path("user_list").post(user_list))
-                .push(Router::new().path("user_save").post(user_save))
-                .push(Router::new().path("user_update").post(user_update))
-                .push(Router::new().path("user_delete").post(user_delete))
-                .push(Router::new().path("update_user_password").post(update_user_password))
-                .push(Router::new().path("role_list").post(role_list))
-                .push(Router::new().path("role_save").post(role_save))
-                .push(Router::new().path("role_update").post(role_update))
-                .push(Router::new().path("role_delete").post(role_delete))
-                .push(Router::new().path("query_role_menu").post(query_role_menu))
-                .push(Router::new().path("update_role_menu").post(update_role_menu))
-                .push(Router::new().path("menu_list").post(menu_list))
-                .push(Router::new().path("menu_save").post(menu_save))
-                .push(Router::new().path("menu_update").post(menu_update))
-                .push(Router::new().path("menu_delete").post(menu_delete))
-        );
 
     let acceptor = TcpListener::new("0.0.0.0:8100").bind().await;
-    Server::new(acceptor).serve(router).await;
+    Server::new(acceptor).serve(route()).await;
+}
+
+fn route() -> Router {
+    Router::new()
+        .path("/api")
+        .get(hello)
+        .push(Router::new().path("login").post(login))
+        .push(
+            Router::new()
+                .hoop(auth_token)
+                .push(build_system_route())
+                .push(build_other_route()),
+        )
 }
